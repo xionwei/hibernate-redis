0c81d0be39aa7ba6c9d35ae057dc6d889a2763e7
xiongwei <306483092@qq.com>
2017-06-14 11:20:54 +0800

    pack

--- hibernate-redis-client/src/main/java/org/hibernate/cache/redis/client/RedisClientFactory.java
+++ hibernate-redis-client/src/main/java/org/hibernate/cache/redis/client/RedisClientFactory.java
@@ -18,13 +18,15 @@ package org.hibernate.cache.redis.client;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.cache.redis.util.RedisCacheUtil;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.SnappyCodec;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 
 import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URL;
 
 /**
